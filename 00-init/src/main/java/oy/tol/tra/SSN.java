a1b16c995bba2e5c17c1bb9ee19b26480339b9d3
zealot6 <2085740661@qq.com>
2024-02-28 11:19:32 +0800

    00

--- 00-init/src/main/java/oy/tol/tra/SSN.java
+++ 00-init/src/main/java/oy/tol/tra/SSN.java
@@ -100,7 +100,8 @@ public class SSN {
                String personNumberString = ssn.substring(PERSON_CODE_INDEX_START,
                      PERSON_CODE_INDEX_START + PERSON_CODE_LENGTH);
                // Next checking the three digit number after the century separator.
-               Integer personNumber = Integer.parseInt(personNumberString);
+               String numericString = personNumberString.replaceAll("[^0-9]", "");
+               Integer personNumber = Integer.parseInt(numericString);
                if (personNumber > 0) {
                   // If it was a positive integer, then calculate the checksum.
                   StringBuilder builder = new StringBuilder();
