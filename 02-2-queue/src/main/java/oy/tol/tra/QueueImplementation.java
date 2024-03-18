6c0283bf950be5796bc65c6a029ebb396d836233
zealot6 <126141707+zealot6@users.noreply.github.com>
2024-03-18 18:05:36 +0800

    Update QueueImplementation.java

--- 02-2-queue/src/main/java/oy/tol/tra/QueueImplementation.java
+++ 02-2-queue/src/main/java/oy/tol/tra/QueueImplementation.java
@@ -1,5 +1,5 @@
 package oy.tol.tra;
-
+import java.util.ArrayList;
 /**
  * An implementation of the StackInterface.
  * <p>
@@ -11,43 +11,42 @@ package oy.tol.tra;
  * - StackImplementation(int size), which allocates an array of Object's with size.
  *  -- remember to maintain the capacity and/or currentIndex when the stack is manipulated.
  */
-public class StackImplementation<E> implements StackInterface<E> {
-
+public class QueueImplementation<E> implements QueueInterface<E> {
+   private ArrayList<E> arr;
    private Object [] itemArray;
+   private int first;
+   private int next;
+   private int size;
    private int capacity;
-   private static final int MIN_QUEUE_SIZE = 10;
-   private static final int MAX_QUEUE_SIZE = 100;
-   private static int toAdd = 0;
    /**
     * Allocates a stack with a default capacity.
     * @throws StackAllocationException
     */
-   
-   /** TODO: Implement so that
-    * - if the size is less than 2, throw StackAllocationException
-    * - if the allocation of the array throws with Java exception,
-    *   throw StackAllocationException.
-    * @param capacity The capacity of the stack.
-    * @throws StackAllocationException If cannot allocate room for the internal array.
-    */
-   public StackImplementation(int capacity) throws StackAllocationException {
-      this.capacity=capacity;
-      
-     
-      if (capacity < 2) {
-         throw new StackAllocationException("The capacity is too small");
-     }
-     
-     try{
-         itemArray = new Object[capacity];
-     }
-     catch(Exception e)
-     {
-      throw new StackAllocationException("Space cannot be allocated");
-     }
+   public QueueImplementation(int capacity) throws QueueAllocationException {
+      // TODO: call the constructor with size parameter with default size of 10.
+      first = 0;
+      next =-1;  
+      this.arr = new ArrayList<>(capacity);
+      size = 0;
+      this.capacity = capacity;
    }
-         
    
+
+   
+         
+   private void reallocate(int newCapacity) {
+      ArrayList<E> newArray = new ArrayList<>(newCapacity);
+     
+      for (int i = 0; i < size; i++) {
+          int index = (first + i) % capacity;
+          newArray.add(arr.get(index));
+      }
+  
+      arr = newArray;
+      first = 0;
+      next = size - 1;
+      capacity = newCapacity;
+  }
 
    @Override
    public int capacity() {
@@ -58,88 +57,82 @@ public class StackImplementation<E> implements StackInterface<E> {
    @Override
    public void enqueue(E element) throws QueueAllocationException, NullPointerException {
       // TODO: Implement this
-         if(element==null)
-         {
-            throw new NullPointerException("Error");
-         }
-          if(top==capacity-1)
-         {
-            largeCapacity(capacity * 2); 
-         }
-         
-            top++;
-         itemArray[top] = element;
-         currentIndex++;
-         
-              
+      if (element == null) {
+         throw new NullPointerException("the element is wrong");
+     }
+     if (size == capacity) {
+         reallocate(capacity * 2);
+     }
+     next = (next + 1) % capacity;
+     if (next == arr.size()) {
+         arr.add(element);
+     }
+     else{
+         arr.set(next, element);
+     }
+     size++;
    }
-   private void largeCapacity(int Capacity1) {
-      Object[] Array1 = new Object[Capacity1];
-      for (int i = 0; i < capacity; i++) {
-          Array1[i] = itemArray[i];
-      }
-      
-      itemArray = Array1;
-      capacity = Capacity1;
-  }
+
 
    @SuppressWarnings("unchecked")
    @Override
-   public E dequeue() throws QueueIsEmptyException{
-      if(top==-1)
-      {
-          throw new StackIsEmptyException("Stack is empty");
-      }
-      else{
-       top--;
-       currentIndex--;
-      return (E)itemArray[top+1];
-      }
+   public E dequeue() throws QueueIsEmptyException {
+      if (isEmpty()) {
+         throw new QueueIsEmptyException("Queue is empty.");
+     }
+
+     E item = arr.get(first);
+     first = (first + 1) % capacity;
+     size--;
+     
+
+     if (size == 0) {
+         first = 0;
+         next = -1;
+     }
+
+     return item;
 
    }
 
    @SuppressWarnings("unchecked")
    @Override
-   public E peek() throws StackIsEmptyException {
-      if(top==-1)
-      {
-         throw new StackIsEmptyException("Stack is empty.");
-      }
-      else{
-      return (E)itemArray[top];
-   }
+   public E element() throws QueueIsEmptyException {
+      if (next == -1) {
+         throw new QueueIsEmptyException("Queue is empty.");
+     }
+     return arr.get(first);
 }
 
-   @Override
-   public int size() {
-      // TODO: Implement this
-      return top+1;
-   }
 
    @Override
    public void clear() {
-      // TODO: Implement this
-      top=-1;
-      currentIndex=-1;
-   }
+      first = 0;
+      next = -1;
+      size = 0;
+  }
 
    @Override
    public boolean isEmpty() {
-      // TODO: Implement this
-     return (top==-1);
+      return (size == 0);
       
    }
 
+   public int size() {
+      return size;
+  }
    @Override
    public String toString() {
       StringBuilder builder = new StringBuilder("[");
-      for (var index = 0; index <= currentIndex; index++) {
-         builder.append(itemArray[index].toString());
-         if (index < currentIndex) {
-            builder.append(", ");
-         }
+      for (int i = 0; i < size; i++) {
+          int index = (i + first) % capacity;
+          builder.append(arr.get(index));
+          if (i < size - 1) {
+              builder.append(", ");
+          }
       }
       builder.append("]");
       return builder.toString();
+  }
    }
-}
+
