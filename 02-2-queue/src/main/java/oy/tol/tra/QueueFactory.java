6fe78afa989876636d147be49f552296cb53f8b0
zealot6 <126141707+zealot6@users.noreply.github.com>
2024-03-18 18:06:20 +0800

    Update QueueFactory.java

--- 02-2-queue/src/main/java/oy/tol/tra/QueueFactory.java
+++ 02-2-queue/src/main/java/oy/tol/tra/QueueFactory.java
@@ -1,13 +1,17 @@
 package oy.tol.tra;
 
+import java.util.ArrayList;
+
+
 /**
  * This class instantiates a queue implementing the {@code QueueInterface}.
  * 
  * @author Antti Juustila
  */
 public class QueueFactory {
-
+   
    private QueueFactory() {
+
    }
 
    /**
@@ -15,25 +19,42 @@ public class QueueFactory {
     * @param capacity Number of elements the queue can hold.
     * @return The queue object.
     */
+    
    public static QueueInterface<Integer> createIntegerQueue(int capacity) {
+     return new QueueImplementation<>(capacity);
+    
+   
+     
+     
+    
       // TODO: Implement this when you have finished your QueueImplementation.
       // - Instantiates your queue implementation using Integer as template parameter, 
       //   with the given capacity,
       // - and return the object to the caller.
-      return new QueueImplementation<>(capacity);
    }
+   
+     
 
+     
+
+   
+      
+  
    /**
     * Creates an instance of QueueInterface for Integer type.
     * Use the default constructor of the queue.
     * @return The queue object.
     */
-   //  public static QueueInterface<Integer> createIntegerQueue() {
+   // 
+    public static QueueInterface<Integer> createIntegerQueue() {
+      return new QueueImplementation<>(10);
    //    // TODO: Implement this when you have finished your QueueImplementation.
    //    // - Instantiates your queue implementation using Integer as template parameter, 
    //    //   with default capacity,
    //    // - and return the object to the caller.
    //    
-   // }
+   //
+   
+ }
 
 }
